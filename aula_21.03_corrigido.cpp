36d642f69895b4eb0e25d740b3399d71079ec646
4snt <murilo.escobedo@protonmail.com>
2024-03-21 19:40:24 -0300

    aiaiiiaiaiai

---
+++ aula_21.03_corrigido.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int NumFatorial, i=1;
+    cout << "Digite o numero a ser feito o fatorial:\n";
+    cin >> NumFatorial;
+    for (NumFatorial; NumFatorial > 1; NumFatorial--) {i *= NumFatorial;}
+    cout << "O resultado do fatorial é:" << i << "\n";
+    return 0;
+}
